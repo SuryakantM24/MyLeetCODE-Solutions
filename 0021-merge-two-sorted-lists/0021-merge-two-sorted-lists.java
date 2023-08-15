412a7e1a4442fd680997895244799ba0be713e83
Suryakant Mudke <suryakantmudke1234@gmail.com>
2023-08-15 22:17:41 +0530

    Time: 0 ms (100%), Space: 41.1 MB (85.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,7 +10,9 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
+
+
+        ListNode head=null,tail=null;
 
         if(list1==null){
             return list2;
@@ -19,13 +21,11 @@ class Solution {
             return list1;
         }
 
-
-        ListNode head=null,tail=null;
-
         if(list1.val<list2.val){
             head=list1;
             tail=list1;
             list1=list1.next;
+
         }
         else{
             head=list2;
@@ -33,30 +33,35 @@ class Solution {
             list2=list2.next;
         }
 
-        while(list1!=null&&list2!=null){
-            if(list1.val<list2.val){
-                tail.next=list1;
-                list1=list1.next;
 
+        while(list1!=null&&list2!=null){
+
+
+            if(list1.val<list2.val){
+                
+                 tail.next=list1;
+                 tail=tail.next;
+                 list1=list1.next;
             }
             else{
-                 tail.next=list2;
-                 list2=list2.next;
-
-
+                tail.next=list2;
+                tail=tail.next;
+                list2=list2.next;
             }
-            tail=tail.next;
         }
-
 
         if(list1!=null){
             tail.next=list1;
+
         }
         if(list2!=null){
             tail.next=list2;
         }
 
         return head;
-                   
+        
     }
 }
+
+
+
