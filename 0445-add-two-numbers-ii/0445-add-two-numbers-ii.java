dfa49db3a1f04faaeb363bd01d1babd8de40c17f
Suryakant Mudke <suryakantmudke1234@gmail.com>
2023-08-18 09:04:17 +0530

    Time: 2 ms (97.13%), Space: 43.1 MB (89.4%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -15,62 +15,57 @@ class Solution {
             return head;
         }
 
-        ListNode curr=head,nextt=head,prev=null;
+        ListNode curr=head,next=head,prev=null;
         while(curr!=null){
-            nextt=nextt.next;
+            next=next.next;
             curr.next=prev;
             prev=curr;
-            curr=nextt;
+            curr=next;
         }
-
         return prev;
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode h1=reverse(l1);
-        ListNode h2=reverse(l2);
+        ListNode temp1=reverse(l1);
+        ListNode temp2=reverse(l2);
+
         int carry=0;
         ListNode head=null,tail=null;
+        while(temp1!=null||temp2!=null){
 
-        while(h1!=null||h2!=null){
             int digit=carry;
-            if(h1!=null){
-                digit+=h1.val;
-                h1=h1.next;
+
+            if(temp1!=null){
+                digit+=temp1.val;
+                temp1=temp1.next;
             }
-            if(h2!=null){
-                digit+=h2.val;
-                h2=h2.next;   
+            if(temp2!=null){
+                digit+=temp2.val;
+                temp2=temp2.next;
             }
+
+            ListNode neww=new ListNode(digit%10);
             carry=digit/10;
-            ListNode temp=new ListNode(digit%10);
+
             if(head==null){
-                head=temp;
-                tail=temp;
+                head=neww;
+                tail=neww;
             }
             else{
-                tail.next=temp;
+                tail.next=neww;
                 tail=tail.next;
             }
-            // if(h1!=null){
-            //     h1=h1.next;
-            // }
-            // if(h2!=null){
-            //     h2=h2.next;
-            //  }
-          
         }
+
         if(carry>0){
-                ListNode temp=new ListNode(carry);
-                tail.next=temp;
-                tail=tail.next;
+             ListNode neww=new ListNode(carry);
+             tail.next=neww;
+             tail=tail.next;
+
         }
-        return reverse(head);   
+
+
+        return reverse(head);
+        
     }
 }
-
-
-
-
-
- 
