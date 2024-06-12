bfcc3bd10bec31c76e73ac8b7c83dcd925ca3985
Suryakant Mudke <suryakantmudke1234@gmail.com>
2024-06-12 20:01:51 +0530

    Time: 0 ms (100%), Space: 43.4 MB (98.1%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,24 +10,20 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-
-
         if(head==null||head.next==null){
             return head;
         }
-        
+
         ListNode temp=head;
-        
         while(temp.next!=null){
-// exit condition should be temp.next!=null as temp.next should be valid to check its data equality.
             if(temp.val==temp.next.val){
-                temp.next=temp.next.next;
+                 temp.next=temp.next.next;
             }
             else{
-                temp=temp.next;
+            temp=temp.next;
             }
         }
-
-        return head;
+        return  head;
+        
     }
 }
