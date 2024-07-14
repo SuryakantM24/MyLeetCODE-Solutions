fb68e54230b5de188adecf57f828cb941f249b79
Suryakant Mudke <suryakantmudke1234@gmail.com>
2024-07-14 16:50:54 +0530

    Time: 4 ms (86.88%), Space: 41.7 MB (58.82%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.java
+++ 0726-number-of-atoms/0726-number-of-atoms.java
@@ -1,70 +1,43 @@
 class Solution {
-    public String countOfAtoms(String formula) {
-        int n = formula.length();
-        Map<String, Integer> result_counter = new HashMap<>();
-        Deque<Map<String, Integer>> parenthesis_stack = new ArrayDeque<>();
-        int cur_ind = 0;
+      public String countOfAtoms(String formula) {
+        Stack<Map<String, Integer>> stack = new Stack<>();
+        stack.push(new HashMap<>());
+        int len = formula.length();
 
-        while (cur_ind < n) {
-            char cur_char = formula.charAt(cur_ind);
-
-            if (cur_char == '(') {
-                cur_ind++;
-                parenthesis_stack.push(new HashMap<>());
-                continue;
-            }
-
-            if (cur_char == ')') {
-                StringBuilder mult_str = new StringBuilder();
-                cur_ind++;
-
-                while (cur_ind < n && Character.isDigit(formula.charAt(cur_ind))) {
-                    mult_str.append(formula.charAt(cur_ind));
-                    cur_ind++;
+        for (int i = 0; i < len; ) {
+            if (formula.charAt(i) == '(') {
+                stack.push(new HashMap<>());
+                i++;
+            } else if (formula.charAt(i) == ')') {
+                Map<String, Integer> top = stack.pop();
+                i++;
+                int start = i;
+                while (i < len && Character.isDigit(formula.charAt(i))) i++;
+                int multiplier = start < i ? Integer.parseInt(formula.substring(start, i)) : 1;
+                for (String key : top.keySet()) {
+                    stack.peek().put(key, stack.peek().getOrDefault(key, 0) + top.get(key) * multiplier);
                 }
-
-                int mult = mult_str.length() == 0 ? 1 : Integer.parseInt(mult_str.toString());
-                Map<String, Integer> last_counter = parenthesis_stack.pop();
-                Map<String, Integer> target = parenthesis_stack.isEmpty() ? result_counter : parenthesis_stack.peek();
-                
-                for (Map.Entry<String, Integer> entry : last_counter.entrySet()) {
-                    target.put(entry.getKey(), target.getOrDefault(entry.getKey(), 0) + entry.getValue() * mult);
-                }
-                continue;
+            } else {
+                int start = i;
+                i++;
+                while (i < len && Character.isLowerCase(formula.charAt(i))) i++;
+                String element = formula.substring(start, i);
+                start = i;
+                while (i < len && Character.isDigit(formula.charAt(i))) i++;
+                int count = start < i ? Integer.parseInt(formula.substring(start, i)) : 1;
+                stack.peek().put(element, stack.peek().getOrDefault(element, 0) + count);
             }
-
-            StringBuilder cur_elem = new StringBuilder();
-            StringBuilder cur_counter_str = new StringBuilder();
-            Map<String, Integer> target = parenthesis_stack.isEmpty() ? result_counter : parenthesis_stack.peek();
-
-            while (cur_ind < n && formula.charAt(cur_ind) != '(' && formula.charAt(cur_ind) != ')') {
-                if (Character.isAlphabetic(formula.charAt(cur_ind))) {
-                    if (Character.isUpperCase(formula.charAt(cur_ind)) && cur_elem.length() > 0) {
-                        target.put(cur_elem.toString(), target.getOrDefault(cur_elem.toString(), 0) + (cur_counter_str.length() == 0 ? 1 : Integer.parseInt(cur_counter_str.toString())));
-                        cur_elem = new StringBuilder();
-                        cur_counter_str = new StringBuilder();
-                    }
-                    cur_elem.append(formula.charAt(cur_ind));
-                } else {
-                    cur_counter_str.append(formula.charAt(cur_ind));
-                }
-                cur_ind++;
-            }
-
-            target.put(cur_elem.toString(), target.getOrDefault(cur_elem.toString(), 0) + (cur_counter_str.length() == 0 ? 1 : Integer.parseInt(cur_counter_str.toString())));
         }
 
-        List<String> parts = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : result_counter.entrySet()) {
-            parts.add(entry.getKey() + (entry.getValue() == 1 ? "" : entry.getValue()));
+        Map<String, Integer> result = stack.pop();
+        List<String> elements = new ArrayList<>(result.keySet());
+        Collections.sort(elements);
+        StringBuilder sb = new StringBuilder();
+        for (String element : elements) {
+            sb.append(element);
+            int count = result.get(element);
+            if (count > 1) sb.append(count);
         }
-        Collections.sort(parts);
-
-        StringBuilder result = new StringBuilder();
-        for (String part : parts) {
-            result.append(part);
-        }
-
-        return result.toString();
+        return sb.toString();
     }
 }
