1b4fcfe88d1a0e61a6b88de95fa8cd2bf05701c2
Suryakant Mudke <suryakantmudke1234@gmail.com>
2025-03-02 14:51:48 +0530

    Time: 47 ms (55.01%), Space: 121.9 MB (19.23%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,80 +1,74 @@
 class LRUCache {
-    class Node{
-        Node next;
-        Node prev;
+
+    class Node {
         int val;
         int key;
-
-        Node( int key,int val){
-            this.val=val;
+        Node prev;
+        Node next;
+        Node (int key,int val){
             this.key=key;
+            this.val=val;
             this.prev=null;
             this.next=null;
         }
-    
+
     }
-    int cap;
+    int capacity;
+    HashMap<Integer,Node> map=new HashMap<Integer,Node>();
     Node head=new Node(-1,-1);
     Node tail=new Node(-1,-1);
-    HashMap<Integer,Node> map=new HashMap<Integer,Node>();
 
     public LRUCache(int capacity) {
-       this.cap=capacity;
-       head.next=tail;
-       tail.prev=head; 
+        this.capacity=capacity;
+        head.next=tail;
+        tail.prev=head;
     }
-
-    private  void addNode (Node newNode){
-        //add start
+    
+    private void addNode(Node adNode){
         Node temp=head.next;
-        newNode.next=temp;
-        newNode.prev=head;
-        head.next=newNode;
-        temp.prev=newNode;
-
+        adNode.next=temp;
+        head.next=adNode;
+        temp.prev=adNode;
+        adNode.prev=head;
     }
-    public void deleteNode(Node delNode){
-        //delete node  -1 head -1 tail solves issue and we always remove from like given postiion
-        Node pre=delNode.prev;
-        Node nex=delNode.next;
+    private void delNode(Node dNode){
+        Node nex=dNode.next;
+        Node pre=dNode.prev;
+
         pre.next=nex;
         nex.prev=pre;
-    }   
-    public int get(int key) {
 
+    }
+    public int get(int key) {
         if(map.containsKey(key)){
             Node res=map.get(key);
-            int out=res.val;
-            map.remove(key);
-            deleteNode(res);
+            int ans=res.val;
+            delNode(res);
             addNode(res);
             map.put(key,head.next);
-            return out;
+            return ans;
         }
-        return -1;   
+        return -1;
+
     }
     
     public void put(int key, int value) {
-        
-        if(map.containsKey(key)){
-           Node res=map.get(key);
-           map.remove(key);
-           deleteNode(res);
 
+        if(map.containsKey(key)){
+            Node res=map.get(key);
+            map.remove(key);
+            delNode(res);
         }
-        if(cap==map.size()){
+        if(capacity==map.size()){
+            Node rem=map.get(tail.prev.key);
             map.remove(tail.prev.key);
-            deleteNode(tail.prev);
+            delNode(rem);
         }
-        Node newNode=new Node(key,value);
-        addNode(newNode);
+        addNode(new Node(key,value));
         map.put(key,head.next);
         
-
-        }
-
     }
-
+}
 
 /**
  * Your LRUCache object will be instantiated and called as such:
@@ -82,18 +76,3 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
